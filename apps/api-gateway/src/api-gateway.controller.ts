adbe46b7d25b9e2b5620784dabf9c40cccb46e34
Pattarapol Chaijam <64160249@go.buu.ac.th>
2024-08-04 00:33:35 +0700

    create controller api-gateway

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiGatewayService } from './api-gateway.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class ApiGatewayController {
-  constructor(private readonly apiGatewayService: ApiGatewayService) {}
-
-  @Get()
-  getHello(): string {
-    return this.apiGatewayService.getHello();
+  constructor(private httpService: HttpService) {}
+  @Get('menu')
+  async getMenu() {
+    const response = await firstValueFrom(
+      this.httpService.get('http://localhost:3001/menu'),
+    );
+    return response.data;
   }
+  @Get('menu/:id')
+  async getMenuItem(@Param('id') id: string) {
+    const response = await firstValueFrom(
+      this.httpService.get(`http://localhost:3001/menu/${id}`),
+    );
+    return response.data;
+  }
+
 }
